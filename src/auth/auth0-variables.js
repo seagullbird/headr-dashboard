e7349a783102a3197df36ff0a33f63a6abb02eb1
seagullbird <seagullbird823@gmail.com>
2018-03-19 14:20:48 +0800

    updated auth0 variables

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,7 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'VUpXGbg7KhvcKKcCOPuPGOpHMy2sYzRd',
   domain: 'headr.auth0.com',
-  audience: 'headr-api',
-  callbackUrl: process.env.BASE_URL + '/callback',
-  apiUrl: 'headr-api'
+  audience: 'https://api.headr.io',
+  callbackUrl: process.env.BASE_URL + '/callback'
 }
