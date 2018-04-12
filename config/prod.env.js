44061843e21e8c2535d710fddd0d8aa0eae4f92e
seagullbird <seagullbird823@gmail.com>
2018-04-12 12:36:17 +0800

    added prod BaseAPI URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // GKE cluster apigateway ingress address
+  BASE_API: '"http://35.190.30.250"',
   BASE_URL: '"https://dashboard.headr.io"'
 }
